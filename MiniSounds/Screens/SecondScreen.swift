cf1e0ca63aac3edc5d637fdb30ae8ca62a1fb76a
Daniel Barclay <barclaysd@me.com>
2020-03-09 10:49:30 +0000

    Make AVStatisticsConsumer confirm to NSObject

--- MiniSounds/Screens/SecondScreen.swift
+++ MiniSounds/Screens/SecondScreen.swift
@@ -17,81 +17,69 @@ class SecondScreen: UIViewController {
         
         view.backgroundColor = .white
         
-//        setUpSMP()
+        setUpSMP()
         // Do any additional setup after loading the view.
     }
     
-//    func setUpSMP() {
-//        guard let playerItemProvider = BBCSMPBackingOffMediaSelectorPlayerItemProvider(mediaSelectorClient: MediaSelectorClient(), mediaSet:  "mobile-phone-main", vpid: "bbc_radio_one", artworkFetcher: nil, blacklist: TimeBasedBlacklist(blacklistInterval: 0), connectionResolver: TimeBasedConnectionResolver(), avStatisticsConsumer: AVStatisticsConsumer()) else {
-//            return
-//        }
-//        let player = BBCSMPPlayerBuilder().withPlayerItemProvider(playerItemProvider).build()
-//        player.play()
-//    }
+    func setUpSMP() {
+        guard let playerItemProvider = BBCSMPBackingOffMediaSelectorPlayerItemProvider(mediaSelectorClient: MediaSelectorClient(), mediaSet: "mobile-phone-main", vpid: "bbc_radio_one", artworkFetcher: nil, blacklist: TimeBasedBlacklist(blacklistInterval: 0), connectionResolver: TimeBasedConnectionResolver(), avStatisticsConsumer: AVStatisticsConsumer()) else {
+            return
+        }
+        let player = BBCSMPPlayerBuilder().withPlayerItemProvider(playerItemProvider).build()
+        player.play()
+    }
 }
 
-//class AVStatisticsConsumer: BBCSMPAVStatisticsConsumer {
-//    func `self`() -> Self {
-//        return self
-//    }
-//
-//    func trackAVSessionStart(itemMetadata: BBCSMPItemMetadata!) {}
-//
-//    func trackAVFullMediaLength(lengthInSeconds mediaLengthInSeconds: Int) {}
-//
-//    func trackAVPlayback(currentLocation: Int, customParameters: [AnyHashable: Any]!) {}
-//
-//    func trackAVPlaying(subtitlesActive: Bool, playlistTime: Int, assetTime: Int, currentLocation: Int, assetDuration: Int) {}
-//
-//    func trackAVBuffer(playlistTime: Int, assetTime: Int, currentLocation: Int) {}
-//
-//    func trackAVPause(playlistTime: Int, assetTime: Int, currentLocation: Int) {}
-//
-//    func trackAVResume(playlistTime: Int, assetTime: Int, currentLocation: Int) {}
-//
-//    func trackAVScrub(from fromTime: Int, to toTime: Int) {}
-//
-//    func trackAVEnd(subtitlesActive: Bool, playlistTime: Int, assetTime: Int, assetDuration: Int, wasNatural: Bool, customParameters: [AnyHashable: Any]!) {}
-//
-//    func trackAVSubtitlesEnabled(_ subtitlesEnabled: Bool) {}
-//
-//    func trackAVPlayerSizeChange(_ playerSize: CGSize) {}
-//
-//    func trackAVError(_ errorString: String!, playlistTime: Int, assetTime: Int, currentLocation: Int, customParameters: [AnyHashable: Any]!) {}
-//
-//    func isEqual(_ object: Any?) -> Bool {
-//        return true
-//    }
-//
-//    var hash: Int = 0
-//
-//    var superclass: AnyClass?
-//
-//    func perform(_ aSelector: Selector!) -> Unmanaged<AnyObject>! {}
-//
-//    func perform(_ aSelector: Selector!, with object: Any!) -> Unmanaged<AnyObject>! {}
-//
-//    func perform(_ aSelector: Selector!, with object1: Any!, with object2: Any!) -> Unmanaged<AnyObject>! {}
-//
-//    func isProxy() -> Bool {
-//        return true
-//    }
-//
-//    func isKind(of aClass: AnyClass) -> Bool {
-//        return true
-//    }
-//
-//    func isMember(of aClass: AnyClass) -> Bool {
-//        return true
-//    }
-//
-//    func conforms(to aProtocol: Protocol) -> Bool {
-//        return true
-//    }
-//
-//    func responds(to aSelector: Selector!) -> Bool {
-//        return true
-//    }
-//
-//    var description: String = ""
-//}
+class AVStatisticsConsumer: NSObject, BBCSMPAVStatisticsConsumer {
+    override func `self`() -> Self {
+        return self
+    }
+    
+    func trackAVSessionStart(itemMetadata: BBCSMPItemMetadata!) {}
+    
+    func trackAVFullMediaLength(lengthInSeconds mediaLengthInSeconds: Int) {}
+    
+    func trackAVPlayback(currentLocation: Int, customParameters: [AnyHashable: Any]!) {}
+    
+    func trackAVPlaying(subtitlesActive: Bool, playlistTime: Int, assetTime: Int, currentLocation: Int, assetDuration: Int) {}
+    
+    func trackAVBuffer(playlistTime: Int, assetTime: Int, currentLocation: Int) {}
+    
+    func trackAVPause(playlistTime: Int, assetTime: Int, currentLocation: Int) {}
+    
+    func trackAVResume(playlistTime: Int, assetTime: Int, currentLocation: Int) {}
+    
+    func trackAVScrub(from fromTime: Int, to toTime: Int) {}
+    
+    func trackAVEnd(subtitlesActive: Bool, playlistTime: Int, assetTime: Int, assetDuration: Int, wasNatural: Bool, customParameters: [AnyHashable: Any]!) {}
+    
+    func trackAVSubtitlesEnabled(_ subtitlesEnabled: Bool) {}
+    
+    func trackAVPlayerSizeChange(_ playerSize: CGSize) {}
+    
+    func trackAVError(_ errorString: String!, playlistTime: Int, assetTime: Int, currentLocation: Int, customParameters: [AnyHashable: Any]!) {}
+    
+    override func isEqual(_ object: Any?) -> Bool {
+        return true
+    }
+        
+    override func isProxy() -> Bool {
+        return true
+    }
+    
+    override func isKind(of aClass: AnyClass) -> Bool {
+        return true
+    }
+    
+    override func isMember(of aClass: AnyClass) -> Bool {
+        return true
+    }
+    
+    override func conforms(to aProtocol: Protocol) -> Bool {
+        return true
+    }
+    
+    override func responds(to aSelector: Selector!) -> Bool {
+        return true
+    }
+}
